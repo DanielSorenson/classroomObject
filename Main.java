fe245322d1c84d6731903fb3a49bb18b37128754
DanielSorenson <96800597+DanielSorenson@users.noreply.github.com>
2022-09-19 13:17:51 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,16 +1,27 @@
 class Main {
   public static void main(String[] args) {
 
-
+    Projector b = new Projector();
     Projector a = new Projector(8);
-
-    
-    a.lowerOrHigher(6);
+    Projector c = new Projector(2, false, false);
 
   
-    
-  
 
+    a.turnOn();
+    b.turnOn();
+    c.turnOn();
+
+    a.hdmi("My Pc");
+    b.hdmi("My iPhone");
+    c.hdmi("Apple TV");
+
+    a.lowerOrHigher(2);
+    b.lowerOrHigher(-2);
+    c.lowerOrHigher(2);
+
+    a.turnOff();
+    b.turnOff();
+    c.turnOff();
 
     
   }
